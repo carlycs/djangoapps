953abf3fa1b6da5d59759c2050b6a5938017669f
office206 <hp@lotustech.io>
2016-11-19 14:51:27 -0500

    static file upload

--- polls/tests.py
+++ polls/tests.py
@@ -7,15 +7,34 @@ from . models import Question
 
 # Create your tests here.
 class QuestionMethodTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
     def test_was_published_recently_with_old_question(self):
-    """
-    was_published_recently() should return False for questions whose
-    pub_date is older than 1 day.
-    """
-    time = timezone.now() - datetime.timedelta(days=30)
-    old_question = Question(pub_date=time)
-    self.assertIs(old_question.was_published_recently(), False)
-    
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+        
     
     def test_was_published_recently_with_future_question(self):
         """
